30fa624054ec5ddaadf88ee09512d919eea37dd0
mazzasj <113390032+mazzasj@users.noreply.github.com>
2023-03-20 13:21:48 -0400

    Add files via upload

--- radar3.py
+++ radar3.py
@@ -1,6 +1,9 @@
 import machine
 import time
 
+data_file80 = open("data80.txt", "w")
+data_file81 = open("data81.txt", "w")
+
 # Set the TX and RX pins
 tx_pin = machine.Pin(16)
 rx_pin = machine.Pin(17)
@@ -20,7 +23,6 @@ def checksum_calc(packet): #calculate the checksum of the packet and add it to t
     packet.append(checksum)
     packet.append(0x54) #add tailings
     packet.append(0x43)
-
     return packet
 
 def good_checksum(packet): #check if the checksum is correct
@@ -51,9 +53,13 @@ def send(packet):
     checksum_calc(packet)
     uart.write(packet)
 
+def strip_header(packet):
+    packet = packet[2:-3]
+    return packet    
 
 
-responses = set()
+
+#responses = set() 
 frame_buffer = []
 time_last = 0
 
@@ -61,8 +67,10 @@ human_presence_switch(0)
 
 time.sleep(1)
 
+time_start = time.ticks_ms()
+
 while True:
-    # Wait for a response
+    # Wait for a response from the RaDAR
     response = uart.read(1)
     if response:
         frame_buffer.append(response[0])
@@ -70,21 +78,33 @@ while True:
             for i in range(len(frame_buffer)): #find the beginning of the frame
                 if frame_buffer[i] == 0x53 and frame_buffer[i+1] == 0x59:
                     good_checksum(frame_buffer) #check received packet checksum
-                    #print(list(map(hex,frame_buffer))) #print the entire frame in hex format
-                    if int(frame_buffer[-4]) != 1 and int(frame_buffer[-4]) != 0:
-                        #print(hex(frame_buffer[2]))
-                        if frame_buffer[2] == 0x80: # and int(frame_buffer[3]) == 1):
-                            print("=========================================")
-                            print("control: ", hex(frame_buffer[2]))
-                            print("command: ", hex(frame_buffer[3]))
-                            print("data: ", int(frame_buffer[-4]))
+                    frame_buffer = strip_header(frame_buffer) #remove header and checksum for readability
+                    #print(list(map(hex, frame_buffer)), frame_buffer[-1]) #print the frame and final data bit in decimal
+                    if frame_buffer[0] == 0x81 and frame_buffer[1] == 0x3: # filter this packet type so its data can be logged
+                        #print("=========================================")
+                        #print("control: ", hex(frame_buffer[0]))
+                        #print("command: ", hex(frame_buffer[1]))
+                        print("data: ", int(frame_buffer[-1]))
+                        event_time = time.ticks_ms() - time_start
+                        data = str(int(frame_buffer[-1])) + " " + str(event_time) + "\n"
+                        data_file81.write(data)
+                        data_file81.flush()
+                    if frame_buffer[0] == 0x80 and frame_buffer[1] == 0x3:
+                        print("data: ", int(frame_buffer[-1]))
+                        event_time = time.ticks_ms() - time_start
+                        data = str(int(frame_buffer[-1])) + " " + str(event_time) + "\n"
+                        data_file80.write(data)
+                        data_file80.flush()
+
                 frame_buffer.clear()
-                time_now = time.time()
-                if time_now - time_last > 10:
-                    send(hb_query)
-                    time_last = time_now
-                    print("sent heartbeat query")
+                #time_now = time.time()
+                #if time_now - time_last > 10:
+                #    send(hb_query)
+                #    time_last = time_now
+                    #print("sent heartbeat query")
                 break
                     
+# look into [0x81 0x3] and [0x80 0x3]
+#[0x81 0x3] appears to be a movement detection packet which scales with the amount of movement
 
-        
+#[0x80 0x3] appears to be a respiration movement packet with possible heartbeat data
